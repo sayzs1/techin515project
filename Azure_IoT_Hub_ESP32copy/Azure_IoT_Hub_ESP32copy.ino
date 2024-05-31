eb7ba8462be3c70fb94371e8bab51dde5d68f721
Jessijiaqi <148395825+Jessijiaqi@users.noreply.github.com>
2024-05-31 00:32:22 -0700

    Add files via upload

---
+++ Azure_IoT_Hub_ESP32copy/Azure_IoT_Hub_ESP32copy.ino
@@ -0,0 +1,413 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// SPDX-License-Identifier: MIT
+
+/*
+ * This is an Arduino-based Azure IoT Hub sample for ESPRESSIF ESP32 boards.
+ * It uses our Azure Embedded SDK for C to help interact with Azure IoT.
+ * For reference, please visit https://github.com/azure/azure-sdk-for-c.
+ *
+ * To connect and work with Azure IoT Hub you need an MQTT client, connecting, subscribing
+ * and publishing to specific topics to use the messaging features of the hub.
+ * Our azure-sdk-for-c is an MQTT client support library, helping composing and parsing the
+ * MQTT topic names and messages exchanged with the Azure IoT Hub.
+ *
+ * This sample performs the following tasks:
+ * - Synchronize the device clock with a NTP server;
+ * - Initialize our "az_iot_hub_client" (struct for data, part of our azure-sdk-for-c);
+ * - Initialize the MQTT client (here we use ESPRESSIF's esp_mqtt_client, which also handle the tcp
+ * connection and TLS);
+ * - Connect the MQTT client (using server-certificate validation, SAS-tokens for client
+ * authentication);
+ * - Periodically send telemetry data to the Azure IoT Hub.
+ *
+ * To properly connect to your Azure IoT Hub, please fill the information in the `iot_configs.h`
+ * file.
+ */
+
+// C99 libraries
+#include <cstdlib>
+#include <string.h>
+#include <time.h>
+
+// Libraries for MQTT client and WiFi connection
+#include <WiFi.h>
+#include <mqtt_client.h>
+
+// Azure IoT SDK for C includes
+#include <az_core.h>
+#include <az_iot.h>
+#include <azure_ca.h>
+
+// Additional sample headers
+#include "AzIoTSasToken.h"
+#include "SerialLogger.h"
+#include "iot_configs.h"
+
+#include "Wire.h"
+#include "I2Cdev.h"
+#include "MPU6050_6Axis_MotionApps612.h"
+#include <PulseSensorPlayground.h>  // Includes the PulseSensorPlayground Library.
+
+const int PulseWire = 2;  // PulseSensor PURPLE WIRE connected to ANALOG PIN 2
+int Threshold = 550;      // Signal threshold for beat detection
+PulseSensorPlayground pulseSensor;  // Creates an instance of the PulseSensorPlayground object
+
+// MPU6050 wiring and address configurations
+MPU6050 mpu68(0x68);  // Device 0x68
+MPU6050 mpu69(0x69);  // Device 0x69
+
+// Error code for device operations
+uint8_t error_code = 0U;
+
+// Quaternions for orientation data from the sensors
+Quaternion q68;
+Quaternion q69;
+
+// Buffer for the DMP data
+uint8_t fifo_buffer68[64]; // FIFO storage buffer for device 0x68
+uint8_t fifo_buffer69[64]; // FIFO storage buffer for device 0x69
+
+// When developing for your own Arduino-based platform,
+// please follow the format '(ard;<platform>)'.
+#define AZURE_SDK_CLIENT_USER_AGENT "c%2F" AZ_SDK_VERSION_STRING "(ard;esp32)"
+
+// Utility macros and defines
+#define sizeofarray(a) (sizeof(a) / sizeof(a[0]))
+#define NTP_SERVERS "pool.ntp.org", "time.nist.gov"
+#define MQTT_QOS1 1
+#define DO_NOT_RETAIN_MSG 0
+#define SAS_TOKEN_DURATION_IN_MINUTES 60
+#define UNIX_TIME_NOV_13_2017 1510592825
+
+#define PST_TIME_ZONE -8
+#define PST_TIME_ZONE_DAYLIGHT_SAVINGS_DIFF 1
+
+#define GMT_OFFSET_SECS (PST_TIME_ZONE * 3600)
+#define GMT_OFFSET_SECS_DST ((PST_TIME_ZONE + PST_TIME_ZONE_DAYLIGHT_SAVINGS_DIFF) * 3600)
+
+// Translate iot_configs.h defines into variables used by the sample
+static const char* ssid = IOT_CONFIG_WIFI_SSID;
+static const char* password = IOT_CONFIG_WIFI_PASSWORD;
+static const char* host = IOT_CONFIG_IOTHUB_FQDN;
+static const char* mqtt_broker_uri = "mqtts://" IOT_CONFIG_IOTHUB_FQDN;
+static const char* device_id = IOT_CONFIG_DEVICE_ID;
+static const int mqtt_port = AZ_IOT_DEFAULT_MQTT_CONNECT_PORT;
+
+// Memory allocated for the sample's variables and structures.
+static esp_mqtt_client_handle_t mqtt_client;
+static az_iot_hub_client client;
+
+static char mqtt_client_id[128];
+static char mqtt_username[128];
+static char mqtt_password[200];
+static uint8_t sas_signature_buffer[256];
+static unsigned long next_telemetry_send_time_ms = 0;
+static char telemetry_topic[128];
+static uint32_t telemetry_send_count = 0;
+static String telemetry_payload = "{}";
+
+#define INCOMING_DATA_BUFFER_SIZE 128
+static char incoming_data[INCOMING_DATA_BUFFER_SIZE];
+
+// Auxiliary functions
+#ifndef IOT_CONFIG_USE_X509_CERT
+static AzIoTSasToken sasToken(
+    &client,
+    AZ_SPAN_FROM_STR(IOT_CONFIG_DEVICE_KEY),
+    AZ_SPAN_FROM_BUFFER(sas_signature_buffer),
+    AZ_SPAN_FROM_BUFFER(mqtt_password));
+#endif // IOT_CONFIG_USE_X509_CERT
+
+static void connectToWiFi()
+{
+  Logger.Info("Connecting to WIFI SSID " + String(ssid));
+
+  WiFi.mode(WIFI_STA);
+  WiFi.disconnect();
+  delay(100);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+
+  Logger.Info("WiFi connected, IP address: " + WiFi.localIP().toString());
+}
+
+static void initializeTime()
+{
+  Logger.Info("Setting time using SNTP");
+
+  configTime(GMT_OFFSET_SECS, GMT_OFFSET_SECS_DST, NTP_SERVERS);
+  time_t now = time(NULL);
+  while (now < UNIX_TIME_NOV_13_2017)
+  {
+    delay(500);
+    Serial.print(".");
+    now = time(nullptr);
+  }
+  Serial.println("");
+  Logger.Info("Time initialized!");
+}
+
+void receivedCallback(char* topic, byte* payload, unsigned int length)
+{
+  Logger.Info("Received [");
+  Logger.Info(topic);
+  Logger.Info("]: ");
+  for (int i = 0; i < length; i++)
+  {
+    Serial.print((char)payload[i]);
+  }
+  Serial.println("");
+}
+
+static esp_err_t mqtt_event_handler(esp_mqtt_event_handle_t event)
+{
+  switch (event->event_id)
+  {
+    int i, r;
+
+    case MQTT_EVENT_ERROR:
+      Logger.Info("MQTT event MQTT_EVENT_ERROR");
+      break;
+    case MQTT_EVENT_CONNECTED:
+      Logger.Info("MQTT event MQTT_EVENT_CONNECTED");
+
+      r = esp_mqtt_client_subscribe(mqtt_client, AZ_IOT_HUB_CLIENT_C2D_SUBSCRIBE_TOPIC, 1);
+      if (r == -1)
+      {
+        Logger.Error("Could not subscribe for cloud-to-device messages.");
+      }
+      else
+      {
+        Logger.Info("Subscribed for cloud-to-device messages; message id:" + String(r));
+      }
+
+      break;
+    case MQTT_EVENT_DISCONNECTED:
+      Logger.Info("MQTT event MQTT_EVENT_DISCONNECTED");
+      break;
+    case MQTT_EVENT_SUBSCRIBED:
+      Logger.Info("MQTT event MQTT_EVENT_SUBSCRIBED");
+      break;
+    case MQTT_EVENT_UNSUBSCRIBED:
+      Logger.Info("MQTT event MQTT_EVENT_UNSUBSCRIBED");
+      break;
+    case MQTT_EVENT_PUBLISHED:
+      Logger.Info("MQTT event MQTT_EVENT_PUBLISHED");
+      break;
+    case MQTT_EVENT_DATA:
+      Logger.Info("MQTT event MQTT_EVENT_DATA");
+
+      for (i = 0; i < (INCOMING_DATA_BUFFER_SIZE - 1) && i < event->topic_len; i++)
+      {
+        incoming_data[i] = event->topic[i];
+      }
+      incoming_data[i] = '\0';
+      Logger.Info("Topic: " + String(incoming_data));
+
+      for (i = 0; i < (INCOMING_DATA_BUFFER_SIZE - 1) && i < event->data_len; i++)
+      {
+        incoming_data[i] = event->data[i];
+      }
+      incoming_data[i] = '\0';
+      Logger.Info("Data: " + String(incoming_data));
+
+      break;
+    case MQTT_EVENT_BEFORE_CONNECT:
+      Logger.Info("MQTT event MQTT_EVENT_BEFORE_CONNECT");
+      break;
+    default:
+      Logger.Error("MQTT event UNKNOWN");
+      break;
+  }
+
+  return ESP_OK;
+}
+
+static void initializeIoTHubClient()
+{
+  az_iot_hub_client_options options = az_iot_hub_client_options_default();
+  options.user_agent = AZ_SPAN_FROM_STR(AZURE_SDK_CLIENT_USER_AGENT);
+
+  if (az_result_failed(az_iot_hub_client_init(
+          &client,
+          az_span_create((uint8_t*)host, strlen(host)),
+          az_span_create((uint8_t*)device_id, strlen(device_id)),
+          &options)))
+  {
+    Logger.Error("Failed initializing Azure IoT Hub client");
+    return;
+  }
+
+  size_t client_id_length;
+  if (az_result_failed(az_iot_hub_client_get_client_id(
+          &client, mqtt_client_id, sizeof(mqtt_client_id) - 1, &client_id_length)))
+  {
+    Logger.Error("Failed getting client id");
+    return;
+  }
+
+  if (az_result_failed(az_iot_hub_client_get_user_name(
+          &client, mqtt_username, sizeofarray(mqtt_username), NULL)))
+  {
+    Logger.Error("Failed to get MQTT clientId, return code");
+    return;
+  }
+
+  Logger.Info("Client ID: " + String(mqtt_client_id));
+  Logger.Info("Username: " + String(mqtt_username));
+}
+
+static int initializeMqttClient()
+{
+#ifndef IOT_CONFIG_USE_X509_CERT
+  if (sasToken.Generate(SAS_TOKEN_DURATION_IN_MINUTES) != 0)
+  {
+    Logger.Error("Failed generating SAS token");
+    return 1;
+  }
+#endif
+
+  esp_mqtt_client_config_t mqtt_config;
+  memset(&mqtt_config, 0, sizeof(mqtt_config));
+  mqtt_config.uri = mqtt_broker_uri;
+  mqtt_config.port = mqtt_port;
+  mqtt_config.client_id = mqtt_client_id;
+  mqtt_config.username = mqtt_username;
+
+#ifdef IOT_CONFIG_USE_X509_CERT
+  Logger.Info("MQTT client using X509 Certificate authentication");
+  mqtt_config.client_cert_pem = IOT_CONFIG_DEVICE_CERT;
+  mqtt_config.client_key_pem = IOT_CONFIG_DEVICE_CERT_PRIVATE_KEY;
+#else // Using SAS key
+  mqtt_config.password = (const char*)az_span_ptr(sasToken.Get());
+#endif
+
+  mqtt_config.keepalive = 30;
+  mqtt_config.disable_clean_session = 0;
+  mqtt_config.disable_auto_reconnect = false;
+  mqtt_config.event_handle = mqtt_event_handler;
+  mqtt_config.user_context = NULL;
+  mqtt_config.cert_pem = (const char*)ca_pem;
+
+  mqtt_client = esp_mqtt_client_init(&mqtt_config);
+
+  if (mqtt_client == NULL)
+  {
+    Logger.Error("Failed creating mqtt client");
+    return 1;
+  }
+
+  esp_err_t start_result = esp_mqtt_client_start(mqtt_client);
+
+  if (start_result != ESP_OK)
+  {
+    Logger.Error("Could not start mqtt client; error code:" + start_result);
+    return 1;
+  }
+  else
+  {
+    Logger.Info("MQTT client started");
+    return 0;
+  }
+}
+
+/*
+ * @brief           Gets the number of seconds since UNIX epoch until now.
+ * @return uint32_t Number of seconds.
+ */
+static uint32_t getEpochTimeInSecs() { return (uint32_t)time(NULL); }
+
+static void establishConnection()
+{
+  connectToWiFi();
+  initializeTime();
+  initializeIoTHubClient();
+  (void)initializeMqttClient();
+}
+
+static void generateTelemetryPayload(int myBPM) {
+  telemetry_payload = "{\"key\": \"BPM\", \"value\": " + String(myBPM) + "}";
+}
+
+static void sendTelemetry(int myBPM)
+{
+  Logger.Info("Sending telemetry ...");
+
+  // 获取发布主题
+  if (az_result_failed(az_iot_hub_client_telemetry_get_publish_topic(
+          &client, NULL, telemetry_topic, sizeof(telemetry_topic), NULL))) {
+    Logger.Error("Failed az_iot_hub_client_telemetry_get_publish_topic");
+    return;
+  }
+
+  // 生成遥测负载
+  generateTelemetryPayload(myBPM);
+
+  // 发布消息
+  if (esp_mqtt_client_publish(
+          mqtt_client,
+          telemetry_topic,
+          (const char*)telemetry_payload.c_str(),
+          telemetry_payload.length(),
+          MQTT_QOS1,
+          DO_NOT_RETAIN_MSG)
+      == 0) {
+    Logger.Error("Failed publishing");
+  } else {
+    Logger.Info("Message published successfully");
+  }
+}
+
+// Arduino setup and loop main functions.
+
+void setup() {
+  Wire.begin();
+  Wire.setClock(100000);  // Set I2C clock speed to 100kHz
+  Serial.begin(115200);
+
+  // PulseSensor setup
+  pulseSensor.analogInput(PulseWire);  // Configure the PulseSensor object
+  pulseSensor.setThreshold(Threshold);
+
+  if (pulseSensor.begin()) {
+    Serial.println("PulseSensor object created!");  // Confirm object creation
+  }
+
+  establishConnection();  // Establish WiFi and MQTT connection
+}
+
+void loop() {
+  // Check WiFi connection
+  if (WiFi.status() != WL_CONNECTED) {
+    connectToWiFi();
+  }
+#ifndef IOT_CONFIG_USE_X509_CERT
+  else if (sasToken.IsExpired()) {
+    Logger.Info("SAS token expired; reconnecting with a new one.");
+    esp_mqtt_client_destroy(mqtt_client);
+    initializeMqttClient();
+  }
+#endif
+
+  // PulseSensor data
+  if (pulseSensor.sawStartOfBeat()) {  // Check if a beat is detected
+    int myBPM = pulseSensor.getBeatsPerMinute();  // Get BPM
+    Serial.print("BPM: ");
+    Serial.println(myBPM);
+
+    // Generate and send telemetry data with BPM
+    sendTelemetry(myBPM);
+  }
+}
+
+
+
+
+
+
